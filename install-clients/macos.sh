d2e65bc71d4dface5c80c94a04a73df90a984eb2
Floris van der Grinten <floris.vandergrinten@agilebits.com>
2022-10-03 12:36:42 +0200

    Add SSH client install scripts

---
+++ install-clients/macos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Install everything in Brewfile
+brew bundle install -v --file=./Brewfile
---
+++ install-clients/windows.ps1
@@ -0,0 +1,17 @@
+# Install SSH clients
+choco feature enable -n allowGlobalConfirmation
+choco install `
+  git `
+  vscode `
+  github-desktop `
+  cyberduck `
+  tableplus `
+  filezilla `
+  putty `
+  git-fork `
+  gitkraken `
+  sourcetree `
+  sublimemerge `
+  goland `
+  termius `
+  terraform
